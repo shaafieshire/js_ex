9eae7b42f3de1f79d1ad4312a2c378ebd5c3ac9f
shaafieshire <shaafishira@gmail.com>
2026-06-24 12:17:44 +0300

    submited ex 1 & ex 2 & ex 3 & ex4

---
+++ ex 1/main.js
@@ -0,0 +1,12 @@
+// Exercise One js
+let x = 7;
+let y = 3;
+
+
+console.log(x + y); //output: 10
+console.log(x - y); //output: 4
+console.log(x * y); //output: 21 
+console.log(x / y); //output: 2.3333
+console.log(x % y); //output: 1  
+console.log(++x);   //output: 8
+console.log(--y);   //output: 2

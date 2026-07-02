66b75149c4f88737d1110b41bd92ad78ca40628a
shaafieshire <shaafishira@gmail.com>
2026-07-02 10:08:34 +0300

    ex 2

---
+++ ex 2/main.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = "10";
+
+console.log(a == b); // true without type coercion
+console.log(a === b); // false with type coercion
+console.log(a != b); // false without type coercion
+console.log(a !== b); // true with type coercion
+console.log(a > b); // false without type coercion
+console.log(a < b); // false without type coercion
+console.log(a >= b); // true without type coercion
+console.log(a <= b); // true without type coercion

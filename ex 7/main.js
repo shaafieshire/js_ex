6b12a7efb14dc8a68194273ef93c43829566075a
shaafieshire <shaafishira@gmail.com>
2026-07-05 07:33:52 +0300

    ex 7

---
+++ ex 7/main.js
@@ -0,0 +1,13 @@
+// Arrays
+
+let colors = ["red", "green", "blue"];
+
+
+colors.push("yellow")
+console.log(colors) // output: ["red", "green", "blue", "yellow"]
+
+colors.shift();
+console.log(colors); // output: ["green", "blue", "yellow"]
+
+
+console.log(colors.length); // output: 3

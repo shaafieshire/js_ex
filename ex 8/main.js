30a066c58785e9dee95d4541af60a4c5ff1b2522
shaafieshire <shaafishira@gmail.com>
2026-07-05 07:54:05 +0300

    ex 8

---
+++ ex 8/main.js
@@ -0,0 +1,12 @@
+// Object 
+
+let car = {
+    brand: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+
+console.log(car); // output: { brand: "Toyota", model: "Camry", year: 2020 }
+console.log(car["brand"]); // output: "Toyota"
+console.log(car.model); // output: "Camry"
+console.log(car.year); // output: 2020

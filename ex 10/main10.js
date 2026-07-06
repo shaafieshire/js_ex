d0f46c892530cef98152a20f83e6a439f64f47b3
shaafieshire <shaafishira@gmail.com>
2026-07-06 06:39:09 +0300

    ex 10

---
+++ ex 10/main10.js
@@ -0,0 +1,9 @@
+/// Arrays and objects
+
+let library = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+]
+
+console.log(library[0].title); // output: "The Great Gatsby"
+console.log(library[1].author); // output: "Harper Lee"

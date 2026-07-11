7d234df234328e274d11a29951f76a3769460430
shaafieshire <shaafishira@gmail.com>
2026-07-11 06:37:22 +0300

    Ex 13

---
+++ ex 13/main13.js
@@ -0,0 +1,8 @@
+//// while loop
+
+let i = 5;
+
+while(i > 0){
+    console.log(`iteration number ${i}`);
+    i--;
+} // output: iteration number 5, iteration number 4, iteration number 3, iteration number 2, iteration number 1

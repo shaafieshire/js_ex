effa06768aa49a11c8e80b3bd0aa7ccf7c2956da
shaafieshire <shaafishira@gmail.com>
2026-07-12 08:00:34 +0300

    Ex 17

---
+++ ex 17/main17.js
@@ -0,0 +1,18 @@
+/// p25
+
+let temp = -1;
+
+if (temp < 0){
+   console.log("Very cold!");
+} else if ( temp < 15 ){
+   console.log("Cold!");
+} else if (temp < 25){
+   console.log("Warm!");
+} else {
+   console.log("Hot!");
+}
+
+///result 1 = Very cold!
+///result 2 = Cold!
+///result 3 = Warm!
+///result 4 = Hot!

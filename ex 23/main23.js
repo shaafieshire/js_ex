a3b10dbb75535f7849b3c7c336b8fa887d498f0c
shaafieshire <shaafishira@gmail.com>
2026-07-14 07:12:36 +0300

    ex 23

---
+++ ex 23/main23.js
@@ -0,0 +1,10 @@
+//// BRNAD CARS
+
+const brand = {
+   name: "Toyota",
+   model: "Camry",
+   year: 2020
+}
+
+const { name, model, year } = brand;
+console.log(`${name} ${model} ${year}`); // Output: "Toyota Camry 2020"
